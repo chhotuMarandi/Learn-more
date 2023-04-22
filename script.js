4f5af2101748e95715b7f57eb77105e0e44de94f
chhotu marandi <cmarandi29@gmail.com>
2023-04-22 23:52:00 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const ans = document.querySelector(".ans");
+
+// console.log(ans)
+
+const ques = document.querySelector(".questions");
+
+function createAnsElm () {
+ const newDiv = document.createElement("div")
+ newDiv.classList.add("ans");
+console.log(newDiv)
+}
+
+createAnsElm();
