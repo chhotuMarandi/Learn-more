d830e2dc4980fa098d07dd2cdeb9758c97eb3eb8
chhotu marandi <cmarandi29@gmail.com>
2023-04-23 00:36:48 +0530

    create dropdown para with js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const ques = document.querySelector(".questions");
 function createAnsElm () {
  const newDiv = document.createElement("div")
  newDiv.classList.add("ans");
+
+ const hrTage = document.createElement("hr")
+ newDiv.appendChild(hrTage)
+
+ const para = document.createElement("p");
+ para.innerHTML =
+   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo aperiam in exercitationem, totam dolorem odio hic nobis doloremque labore quis libero alias similique magnam quasi cumque culpa dolores sequi voluptates!";
+
+   newDiv.appendChild(para)
 console.log(newDiv)
 }
 
