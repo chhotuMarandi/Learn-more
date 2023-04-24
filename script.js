e19321676832b3aa7864808805c525c34602fc02
chhotu marandi <cmarandi29@gmail.com>
2023-04-24 23:37:57 +0530

    button funtionlaties

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-const ques = document.querySelector(".question-container");
+const btns = document.querySelectorAll(".plusBtn");
 
-const toggleBtn = document.querySelector(".questions i");
+const text = document.querySelectorAll(".text");
 
-
-
-// function createAnsElm () {
-//  const newDiv = document.createElement("div")
-//  newDiv.classList.add("ans");
-
-//  const hrTage = document.createElement("hr")
-//  newDiv.appendChild(hrTage)
-
-//  const para = document.createElement("p");
-//  para.innerHTML =
-//    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo aperiam in exercitationem, totam dolorem odio hic nobis doloremque labore quis libero alias similique magnam quasi cumque culpa dolores sequi voluptates!";
-
-//    newDiv.appendChild(para)
-
-//    ques.appendChild(newDiv);
-// }
-
-
-console.log(ques.children[1].classList.add(""));
-
-toggleBtn.addEventListener("click", () => {
-
+btns.forEach((btn)=> {
+btn.addEventListener("click", () => {
+ 
+})
 })
