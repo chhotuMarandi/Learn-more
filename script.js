2ef718b97068472a52b8cdbef1817d5fc48dd8ef
chhotu marandi <cmarandi29@gmail.com>
2023-04-24 23:59:12 +0530

    toggle funtion is not working

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const btns = document.querySelectorAll(".plusBtn");
 const text = document.querySelectorAll(".text");
 
 btns.forEach((btn)=> {
-btn.addEventListener("click", () => {
- 
+btn.addEventListener("click", (e) => {
+ const btnParent = (e.currentTarget.parentElement.parentElement);
+ btnParent.classList.toggle("show")
 })
 })
