31826bcdf05ac78eb84adc99b7ee570a11768fb5
chhotu marandi <cmarandi29@gmail.com>
2023-04-24 23:08:31 +0530

    restyle

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// const ans = document.querySelector(".ans");
-
 const ques = document.querySelector(".question-container");
 
 const toggleBtn = document.querySelector(".questions i");
 
-// console.log(toggleBtn)
-
-function createAnsElm () {
- const newDiv = document.createElement("div")
- newDiv.classList.add("ans");
-
- const hrTage = document.createElement("hr")
- newDiv.appendChild(hrTage)
-
- const para = document.createElement("p");
- para.innerHTML =
-   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo aperiam in exercitationem, totam dolorem odio hic nobis doloremque labore quis libero alias similique magnam quasi cumque culpa dolores sequi voluptates!";
-
-   newDiv.appendChild(para)
-   // console.log(newDiv)
-
-   ques.appendChild(newDiv);
-}
 
 
+// function createAnsElm () {
+//  const newDiv = document.createElement("div")
+//  newDiv.classList.add("ans");
 
-createAnsElm();
+//  const hrTage = document.createElement("hr")
+//  newDiv.appendChild(hrTage)
+
+//  const para = document.createElement("p");
+//  para.innerHTML =
+//    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo aperiam in exercitationem, totam dolorem odio hic nobis doloremque labore quis libero alias similique magnam quasi cumque culpa dolores sequi voluptates!";
+
+//    newDiv.appendChild(para)
+
+//    ques.appendChild(newDiv);
+// }
+
+
+console.log(ques.children[1].classList.add(""));
+
+toggleBtn.addEventListener("click", () => {
+
+})
